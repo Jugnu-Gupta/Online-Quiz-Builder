4e2ff154f7116238c481744b50f85b55ccadc051
jugnugupta33 <jugnugupta33@gmail.com>
2024-07-02 22:51:11 +0530

    Add MongoDB connection and server setup

---
+++ Backend/src/index.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import mongoose from "mongoose";
+import connectDB from "./db/connectDB.js";
+import { app } from "./app.js";
+
+
+const port = process.env.PORT || 8000;
+connectDB()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server is running at port: ${port}`);
+        })
+    })
+    .catch((err) => {
+        console.log("MONGODB connection Failed!!!");
+    })
