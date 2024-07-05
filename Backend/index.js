4b4e38fc5168f79232246692842ee3a776fee8a4
jugnugupta33 <jugnugupta33@gmail.com>
2024-07-05 16:35:01 +0530

    bug fix

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,8 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import connectDB from "./db/connectDB.js";
-import { app } from "./app.js";
+import connectDB from "./src/db/connectDB.js";
+import { app } from "./src/app.js";
 
 
 const port = process.env.PORT || 8000;
